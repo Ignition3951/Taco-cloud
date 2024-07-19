0b30437b35078bcbb08f7a720de454566463d620
UTKARSH <UTKARSH@DESKTOP-O9PORRS.mshome.net>
2024-07-19 16:55:59 +0530

    Complete business logic for order save without DB

---
+++ taco-cloud/src/main/java/tacos/web/OrderController.java
@@ -0,0 +1,31 @@
+package tacos.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+import lombok.extern.slf4j.Slf4j;
+import tacos.model.TacoOrder;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("tacoOrder")
+public class OrderController {
+	
+	@GetMapping("/current")
+	public String orderForm() {
+		return "orderForm";
+	}
+	
+	@PostMapping
+	public String processOrder(TacoOrder tacoOrder,SessionStatus sessionStatus) {
+		log.info("Order Submitted: {}",tacoOrder);
+		sessionStatus.setComplete();
+		return "redirect:/";
+	}
+
+}
